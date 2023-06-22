464eebf4f08663157ea620b4daa2e80f496585d6
unknown <richardcastuera16@gmail.com>
2023-06-22 14:33:39 +0800

    feat(i_am_poor_app): Added content and image

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(''),
-          backgroundColor: Color(),
+          title: Text('I Am Poor'),
+          centerTitle: true,
+          backgroundColor: Colors.green[400],
         ),
         body: Center(
           child: Image(
-            image: AssetImage(),
+            image: AssetImage('images/nm.png'),
           ),
         ),
       ),
